54e06916d1e9fb52e450f44d96ff3c9c2dc12895
Dumazeau <hdumazeau@richcongress.com>
2021-05-15 16:52:13 +0200

    Add tests

---
+++ Tests/Controller/TourControllerTest.php
@@ -0,0 +1,111 @@
+<?php declare(strict_types=1);
+
+namespace RichId\TourBundle\Tests\Controller;
+
+use RichCongress\TestFramework\TestConfiguration\Annotation\TestConfig;
+use RichCongress\TestSuite\TestCase\ControllerTestCase;
+use RichId\TourBundle\Repository\TourRepository;
+use RichId\TourBundle\Repository\UserTourRepository;
+use RichId\TourBundle\Tests\Resources\Entity\DummyUser;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class TourControllerTest
+ *
+ * @package   RichId\TourBundle\Tests\Controller
+ * @author    Hugo Dumazeau <hugo.dumazeau@rich-id.fr>
+ * @copyright 2014 - 2021 RichId (https://www.rich-id.fr)
+ *
+ * @covers \RichId\TourBundle\Controller\TourController
+ * @TestConfig("fixtures")
+ */
+final class TourControllerTest extends ControllerTestCase
+{
+    /** @var TourRepository */
+    public $tourRepository;
+
+    /** @var UserTourRepository */
+    public $userTourRepository;
+
+    public function testPostEnableNotLogged(): void
+    {
+        $response = $this->getClient()->post('/rich-id-tours/enable', ['tour' => 'database_tour_3']);
+        $this->assertSame(Response::HTTP_FORBIDDEN, $response->getStatusCode());
+    }
+
+    public function testPostEnable(): void
+    {
+        $user = $this->getRepository(DummyUser::class)->find(1);
+        $this->authenticateUser($user);
+
+        $tour = $this->tourRepository->findOneByKeyname('database_tour_3');
+        $this->assertNull($tour);
+
+        $response = $this->getClient()->post('/rich-id-tours/enable', ['tour' => 'database_tour_3']);
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+
+        $tour = $this->tourRepository->findOneByKeyname('database_tour_3');
+        $this->assertNotNull($tour);
+        $this->assertFalse($tour->isDisabled());
+    }
+
+    public function testPostDisableNotLogged(): void
+    {
+        $response = $this->getClient()->post('/rich-id-tours/disable', ['tour' => 'database_tour_3']);
+        $this->assertSame(Response::HTTP_FORBIDDEN, $response->getStatusCode());
+    }
+
+    public function testPostDisable(): void
+    {
+        $user = $this->getRepository(DummyUser::class)->find(1);
+        $this->authenticateUser($user);
+
+        $tour = $this->tourRepository->findOneByKeyname('database_tour_3');
+        $this->assertNull($tour);
+
+        $response = $this->getClient()->post('/rich-id-tours/disable', ['tour' => 'database_tour_3']);
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+
+        $tour = $this->tourRepository->findOneByKeyname('database_tour_3');
+        $this->assertNotNull($tour);
+        $this->assertTrue($tour->isDisabled());
+    }
+
+    public function testPostPerformNotLogged(): void
+    {
+        $response = $this->getClient()->post('/rich-id-tours/perform', ['tour' => 'database_tour_3']);
+        $this->assertSame(Response::HTTP_FORBIDDEN, $response->getStatusCode());
+    }
+
+    public function testPostPerform(): void
+    {
+        $user = $this->getRepository(DummyUser::class)->find(1);
+        $this->authenticateUser($user);
+
+        $tour = $this->tourRepository->findOneByKeyname('database_tour_3');
+        $userTour = $this->userTourRepository->findOneByUserAndTour($user, 'database_tour_3');
+        $this->assertNull($tour);
+        $this->assertNull($userTour);
+
+        $response = $this->getClient()->post('/rich-id-tours/perform', ['tour' => 'database_tour_3']);
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+
+        $tour = $this->tourRepository->findOneByKeyname('database_tour_3');
+        $userTour = $this->userTourRepository->findOneByUserAndTour($user, 'database_tour_3');
+        $this->assertNotNull($tour);
+        $this->assertNotNull($userTour);
+    }
+
+    public function testDeleteResetPerformedTours(): void
+    {
+        $user = $this->getRepository(DummyUser::class)->find(1);
+        $this->authenticateUser($user);
+
+        $this->assertCount(1, $this->userTourRepository->findAll());
+
+        $response = $this->getClient()->delete('/rich-id-tours/perform', ['tour' => 'database_tour_4']);
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+
+        $this->assertEmpty($this->userTourRepository->findAll());
+    }
+}
